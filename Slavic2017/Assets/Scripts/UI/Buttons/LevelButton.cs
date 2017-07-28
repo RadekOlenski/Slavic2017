41cce371bda59af49521161f29543620ac80dc7b
RadekOlenski <radek.olenski@gmail.com>
2017-07-28 23:56:30 +0200

    Add simple canvas in main menu with button to level

---
+++ Slavic2017/Assets/Scripts/UI/Buttons/LevelButton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Enums;
+using Events;
+using Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Buttons
+{
+    public class LevelButton : MonoBehaviour {
+
+        private Button button;
+        private EventManager eventManager;
+
+        // Use this for initialization
+        private void Start()
+        {
+            eventManager = EventManager.Instance;
+            button = gameObject.GetComponent<Button>();
+            button.onClick.AddListener(LoadLevel);
+        }
+
+        private void LoadLevel()
+        {
+            string buttonText = button.GetComponentInChildren<Text>().text;
+            int sceneNumber = Int32.Parse(Regex.Match(buttonText, @"\d+").Value);
+            eventManager.InvokeEvent(new ChangeGameStateEvent(GameState.InGame));
+            eventManager.InvokeEvent(new LoadSceneEvent("Level_0" + sceneNumber));
+            //Uncomment with sounds
+            //EventManager.Instance.InvokeEvent(new PlaySimpleSoundEvent(SoundsEnum.MenuButtonClick));
+        }
+    }
+}
