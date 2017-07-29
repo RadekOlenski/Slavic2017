199d7220ee52d569e33b6418433725aa1285f692
RadekOlenski <radek.olenski@gmail.com>
2017-07-29 15:43:33 +0200

    End level panel

---
+++ Slavic2017/Assets/Scripts/UI/Buttons/NextLevelButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Enums;
+using Events;
+using Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Buttons
+{
+    public class NextLevelButton : MonoBehaviour
+    {
+        private Button button;
+
+        // Use this for initialization
+        void Start()
+        {
+            button = gameObject.GetComponent<Button>();
+            button.onClick.AddListener(GoToNextLevel);
+        }
+
+        private void GoToNextLevel()
+        {
+            try
+            {
+                int sceneIndex = GameManager.Instance.LastLoadedLevel.SceneIndex + 1;
+
+                EventManager.Instance.InvokeEvent(new ChangeGameStateEvent(GameState.InGame));
+
+                EventManager.Instance.InvokeEvent(new LoadSceneByIndexEvent(sceneIndex));
+            }
+            catch (Exception)
+            {
+                Debug.Log("No more scenes!");
+            }
+            //EventManager.Instance.InvokeEvent(new PlaySimpleSoundEvent(SoundsEnum.MenuButtonClick));
+        }
+    }
+}
