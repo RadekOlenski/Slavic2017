15ac34dc925111c959f618b7175b6d62bbbfc038
MarcinJozwik <marty.jozwik@gmail.com>
2017-07-30 08:47:05 +0200

    first iteration of level + postproccessing pack

---
+++ Slavic2017/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Enums;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private GameObject target;
+    private Vector3 offset;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag(TagsEnum.Player);
+        offset = gameObject.transform.position - target.transform.position;
+    }
+
+    void Update()
+    {
+        gameObject.transform.position = target.transform.position + offset;
+    }
+}
